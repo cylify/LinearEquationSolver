0f4956d9e73fea7e2588f359ab23a6f6e0cdd280
cylify <mradi.lachman@gmail.com>
2022-10-24 16:55:31 -0400

    Fixed up constructor, added arrays for left and right side, got operations

--- LinearEquationSolver.java
+++ LinearEquationSolver.java
@@ -6,54 +6,55 @@ class LinearEquation {
     private int equalsIndex;
     private int operatorCount;
     private ArrayList<String> expression = new ArrayList<String>();
+    private ArrayList<String> operators = new ArrayList<String>();
     private ArrayList<String> rightSide = new ArrayList<String>();
     private ArrayList<String> leftSide = new ArrayList<String>();
     private Fraction finalAnswer;
 
 
-    public LinearEquation() {
+    public LinearEquation(String expr) {
 
-    }
-
-    public void GetExpression(String expr) {
         for(String items : expr.split(" ")) {
             this.expression.add(items);
         }
-    }
 
-    public void getEqualIndex() {
-        for(int i = 0; i < this.expression.size(); i++) {
-            if(this.expression.get(i).contains("=")) {
-                this.equalsIndex = this.expression.indexOf("=");
-            }
-        }
-    }
+        this.equalsIndex = expression.indexOf("=");
 
-    public void getRightSide() {
-        for(int i = this.equalsIndex + 1; i < this.expression.size(); i++) {
-            this.rightSide.add(expression.get(i));
-        }
-    }
-
-    public void getLeftside() {
         for(int i = 0; i < this.equalsIndex; i++) {
-            this.leftSide.add(expression.get(i));
+            this.leftSide.add(this.expression.get(i));
+        }
+
+        for(int i = this.equalsIndex + 1; i < expression.size(); i++) {
+            this.rightSide.add(this.expression.get(i));
+        }
+
+        for(int i = 0; i < this.leftSide.size(); i++) {
+            if(this.leftSide.get(i).matches("-{0,1}[0-9]+\\(x")) {
+                this.operatorCount++;
+                this.operators.add("/");
+            } else if(this.leftSide.get(i).matches("-{0,1}[0-9]+x")) {
+                this.operatorCount++;
+                this.operators.add("/");
+            } else if(this.leftSide.get(i).contains("+")) {
+                this.operatorCount++;
+                this.operators.add("-");
+            } else if(this.leftSide.get(i).contains("-")) {
+                this.operatorCount++;
+                this.operators.add("+");
+            } else if(this.leftSide.get(i).matches("-{0,1}[0-9]+/x")) {
+                this.operatorCount++;
+                this.operators.add("*");
+            } 
         }
     }
-
-    public void getOperators() {
-
-    }
-
-    public Fraction evaluate() {
-        
-    }
-
-
 
     
 
+
     public static void main(String[] args) {
-        
+        Scanner in = new Scanner(System.in);
+        String expr = in.nextLine();
+        LinearEquation d = new LinearEquation(expr);
+        in.close();
     }
 }
