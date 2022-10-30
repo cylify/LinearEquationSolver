a458ca0d066ff30366367a766bd13e40fce7aa7c
cylify <mradi.lachman@gmail.com>
2022-10-30 16:06:43 -0400

    Redone

--- LinearEquationSolver.java
+++ LinearEquationSolver.java
@@ -4,142 +4,99 @@ import java.util.*;
 
 class LinearEquation {
     private int equalsIndex;
-    private int operatorCount;
-    private ArrayList<String> expression = new ArrayList<String>();
     private ArrayList<String> operators = new ArrayList<String>();
     private ArrayList<String> rightSide = new ArrayList<String>();
     private ArrayList<String> leftSide = new ArrayList<String>();
-    private ArrayList<String> coeffandnums = new ArrayList<String>();
-    private Fraction finalAnswer;
+    private ArrayList<String> terms = new ArrayList<String>();
+    private ArrayList<String> constants = new ArrayList<String>();
 
 
     /**
      * @param expr
      */
     public LinearEquation(String expr) {
+        GetEqualIndex(expr);
+        GetLeftSide(expr);
+        GetRightSide(expr);
+        System.out.println(this.leftSide);
+        System.out.println(this.rightSide);
 
-        for(String items : expr.split(" ")) {
-            this.expression.add(items);
+
+    }
+
+    public void GetEqualIndex(String expr) {
+        this.equalsIndex = expr.indexOf("=");
+    }
+
+    public void Distribute() {
+        
+    }
+
+    
+    /** 
+     * @param expr
+     */
+    public void GetLeftSide(String expr) {
+        String left = expr.substring(0, this.equalsIndex);
+
+        for (String items : left.split(" ")) {
+            this.leftSide.add(items);
         }
-
-        this.equalsIndex = expression.indexOf("=");
-
-        GetLeftSide(expression);
-
-        GetRightSide(expression);
-
-        GetOperators(leftSide);
-
-        GetCount(operators);
-
-        GetNums(leftSide);
-
-        System.out.print(leftSide + " " + rightSide + " " + operatorCount + " " + operators + " " + coeffandnums);
     }
 
     
     /** 
      * @param expression
      */
-    public void GetLeftSide(ArrayList<String> expression) {
-        for(int i = 0; i < this.equalsIndex; i++) {
-            this.leftSide.add(this.expression.get(i));
+    public void GetRightSide(String expr) {
+        String right = expr.substring(this.equalsIndex + 2);
+
+        for (String items : right.split(" ")) {
+            this.rightSide.add(items);
         }
     }
 
-    
-    /** 
-     * @param expression
-     */
-    public void GetRightSide(ArrayList<String> expression) {
-        for(int i = this.equalsIndex + 1; i < expression.size(); i++) {
-            this.rightSide.add(this.expression.get(i));
+    public void CollectLikeTerms(ArrayList<String> side) {
+        for(int i = 0; i < side.size(); i++) {
+
         }
     }
 
-    
-    /** 
-     * @param leftSide
-     */
-    public void GetOperators(ArrayList<String> leftSide) {
-        for(int i = 0; i < this.leftSide.size(); i++) {
-            if(leftSide.get(i).matches("-{0,1}[0-9]+\\(-{0,1}[1-9]+x")) {
-                this.operators.add("/");
-                this.operators.add("/");
-            } else if(leftSide.get(i).matches("-{0,1}[0-9]+\\(x")) {
-                this.operators.add("/");
-            } else if(leftSide.get(i).matches("-{0,1}[0-9]+x")) {
-                this.operators.add("/");
-            } else if(leftSide.get(i).contains("+")) {
-                this.operators.add("-");
-            } else if(leftSide.get(i).contains("-")) {
-                this.operators.add("+");
-            } else if(leftSide.get(i).matches("-{0,1}[0-9]+/x")) {
-                this.operators.add("*");
-            } else if(leftSide.get(i).matches("-{0,1}[0-9]+\\)\\/x")) {
-                this.operators.add("*");
-            } else if(leftSide.get(i).matches("-{0,1}[0-9]+\\)\\/-{0,1}[0-9]+")) {
-                this.operators.add("*");
-            }
-        }
-    }
-
-    
-    /** 
-     * @param operators
-     */
-    public void GetCount(ArrayList<String> operators) {
-        this.operatorCount = operators.size();
+    public void GetOperators() {
+        
     }
 
 
-    
-    /** 
-     * @param leftSide
-     */
-    public void GetNums(ArrayList<String> leftSide) {
-        for(int i = 0; i < leftSide.size(); i++) {
-            if(leftSide.get(i).matches("-{0,1}[1-9]+")) {
-                coeffandnums.add(leftSide.get(i));
-            }
-        }
-    }
 
-    
-    /** 
-     * @param rightSide
-     * @param coeffandnums
-     */
-    public void Evaluate(ArrayList<String> rightSide, ArrayList<String> coeffandnums) {
-        for(int i = 0; i < rightSide.size(); i++) {
-            
-        }
-    }
-
-
-    /**
-     * @return
-     * @throws IllegalArgumentException
-     */
-    public Fraction valueOf() {
+    public Fraction valueOf(String expr) {
         String mixedFrac = "((-{0,1}[1-9][0-9]*)|-{0,1}0) (([1-9][0-9]*)|-{0,1}0)/[1-9][0-9]*";
         String frac = "((-{0,1}[1-9][0-9]*)|-{0,1}0)/-{0,1}[1-9][0-9]*";
         String wholeFrac = "(-{0,1}[1-9][0-9]*)|-{0,1}0";
-        
-        if(this.rightSide.size() > 1) {
 
-        } else if(this.rightSide.size() == 1) {
-
+        if (expr.matches(mixedFrac)) {
+            int indOfSpace = expr.indexOf(" ");
+            int indOfSlash = expr.indexOf("/");
+            int whole = Integer.valueOf(expr.substring(0, indOfSpace));
+            int num = Integer.valueOf(expr.substring(indOfSpace + 1, indOfSlash));
+            int den = Integer.valueOf(expr.substring(indOfSlash + 1));
+            return new Fraction(whole, num, den);  
+        } else if (expr.matches(frac)) {
+            int indOfSlash = expr.indexOf("/");
+            int num = Integer.valueOf(expr.substring(0, indOfSlash));
+            int den = Integer.valueOf(expr.substring(indOfSlash + 1));
+            return new Fraction(num, den);  
+        } else if (expr.matches(wholeFrac)) {
+            int whole = Integer.valueOf(expr);
+            return new Fraction(whole);
         } else {
-            throw new IllegalArgumentException("Expression must equal a number");
-        }
-
-        if(this.leftSide.size() >= 1) {
-
-        } else {
-            throw new IllegalArgumentException("Must have an expression to check");
+            throw new IllegalArgumentException("Expression does not represent a fraction");
         }
     }
+    
+
+
+
+
 
     
     /** 
