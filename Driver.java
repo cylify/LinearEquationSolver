90be440d21067a54ba84970665dfbd8c0050d660
cylify <mradi.lachman@gmail.com>
2022-11-07 19:08:53 -0500

    Added docstrings

--- Driver.java
+++ Driver.java
@@ -2,8 +2,11 @@ import java.util.Scanner;
 
 class Driver {
 
+    /**
+     * Print menu for user
+     */
     public static void PrintMenu() {
-        String title = "\n\n\n\t\t\t\t\u001b[31m\033[31m Welcome to the Linear Equation Solver! \033[0m";
+        String title = "\n\n\n\t\t\t\t\u001b[31m\033[31m\u001B[4m\u001B[1m Welcome to the Linear Equation Solver! \033[0m\u001B[0m";
         String headline = "\n\n\t\t\t       \u001B[1mChoose what program you would like to use!";
         String calcChoice = "\n\t\t\t\t   1. Linear Equation Calculator";
         String quizChoice = "\n\t\t\t\t   2. Quiz for Linear Equations";
@@ -16,12 +19,50 @@ class Driver {
         System.out.println(spacer);
     }
 
+    /**
+     * Gets choice of what program user wants to run
+     * @param choice
+     * @return
+     * @throws IllegalArgumentException
+     */
     public static int GetChoice(int choice) throws IllegalArgumentException {
+        // Makes sure choice is in bounds
         if(choice > 0 && choice < 3) {
             return choice;
+        // If not in correct boundaries throws IllegalArgumentException
         } else {
             throw new IllegalArgumentException("Not a valid choice");
         }
+    }
+
+    /**
+     * Runs LinearEquation Solver for user
+     */
+    public static void runLinearEquationSolver() {
+        Scanner in = new Scanner(System.in);
+        String expr = in.nextLine();
+        LinearEquation eq = new LinearEquation(expr);
+        System.out.println("x = " + LinearEquation.xVal);
+        in.close();
+    }
+
+    /**
+     * Runs Quiz for user
+     */
+    public static void runQuiz() {
+        Scanner in = new Scanner(System.in);
+        String type = in.nextLine();
+        Quiz q = new Quiz(type);
+        System.out.print("Solve for the unknown in this expression\nx = ");
+        String answer = in.nextLine();
+        boolean flag = Quiz.CheckAnswer(q.getQuestion(), answer);
+        if(flag) {
+            System.out.println("\u001B[32mYou have got the question right!\033[0m");
+        } else {
+            System.out.println("\u001B[31mYou have got the question wrong.\033[0m");
+            System.out.println("\u001b[31mThe correct answer is: " + LinearEquation.xVal);
+        }
+        in.close();
     }
 
     public static void main(String[] args) {
@@ -34,21 +75,11 @@ class Driver {
         if(choiceVal == 1) {
             in.nextLine();
             System.out.print("Enter an expression: ");
-            String expr = in.nextLine();
-            LinearEquation eq = new LinearEquation(expr);
+            runLinearEquationSolver();
         } else if(choiceVal == 2) {
             in.nextLine();
             System.out.print("What type of equation would you solve? (O/T/M): ");
-            String type = in.nextLine();
-            Quiz q = new Quiz(type);
-            System.out.print("Solve for the unknown in this expression\nx = ");
-            String answer = in.nextLine();
-            boolean flag = Quiz.CheckAnswer(Quiz.question, answer);
-            if(flag) {
-                System.out.println("You have got the question right!");
-            } else {
-                System.out.println("You have got the question wrong :(");
-            }
+            runQuiz();
         }
         in.close();
     }
--- Quiz.java
+++ Quiz.java
@@ -1,95 +1,159 @@
 import java.util.*;
 
 class Quiz {
-    static String question;
+    private String question;
 
+    /**
+     * Gets random question for user and prints it
+     * @param type
+     */
     public Quiz(String type) {
         question = GetQuestion(type);
         System.out.println(question);
     }
 
+    /**
+     * For other files to get question
+     * @return String
+     */
+    public String getQuestion() {
+        return question;
+    }
+    
+    /**
+     * Set question
+     * @param question
+     */
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    /**
+     * For choice get question
+     * @param choice
+     * @return String
+     * @throws IllegalArgumentException
+     */
     public String GetQuestion(String choice) throws IllegalArgumentException {
         String question = "";
+        // If choice of user is O then run one step questions
         if(choice.equals("O")) {
             question = OneStepQuestions();
+        // If choice of user is T then run two step questions
         } else if(choice.equals("T")) {
             question = TwoStepQuestions();
+        // If choice of user is M then run multi step questions
         } else if(choice.equals("M")) {
             question = MultiStepQuestions();
+        // If choice does not match these choices, then throw IllegalArgumentException
         } else {
             throw new IllegalArgumentException("Valid choice must be inputted");
         }
         return question;
     }
 
+    /**
+     * Gets random int for question
+     * @return int
+     */
     public int GetRandomInt() {
         Random rand = new Random();
         int num = rand.nextInt(2, 20);
         return num;
     }
 
+    /**
+     * Chance for num
+     * @return int
+     */
     public int GetRandomNumber() {
         Random rand = new Random();
         int chance = rand.nextInt(1, 100);
         return chance;
     }
     
+    /**
+     * Array of all one step types of equations
+     * @return String
+     */
     public String OneStepQuestions() {
         String[] onestep = {(GetRandomNumber() > 50 ? "" : "-") + "%dx = " + (GetRandomNumber() > 50 ? "" : "-") +"%d", 
         (GetRandomNumber() > 50 ? "" : "-") + "x/" + (GetRandomNumber() > 50 ? "" : "-") + "%d = " + 
         (GetRandomNumber() > 50 ? "" : "-") + "%d",
         (GetRandomNumber() > 50 ? "" : "-") + "x " + (GetRandomNumber() > 50 ? "+" : "-") + " " + (GetRandomNumber() > 50 ? "" : "-") + 
         "%d = " + (GetRandomNumber() > 50 ? "" : "-") + "%d"};
+
+        // Formats question with chance of operator and digits
         return String.format(onestep[(new Random()).nextInt(onestep.length)], GetRandomInt(), GetRandomInt());
     }
 
+    /**
+     * Array of all two step types of equations
+     * @return String
+     */
     public String TwoStepQuestions() {
         String[] twostep = {(GetRandomNumber() > 50 ? "" : "-") + "%dx " + (GetRandomNumber() > 50 ? "+" : "-") + " " + 
-                            (GetRandomNumber() > 50 ? "" : "-") + "%d = " + (GetRandomNumber() > 50 ? "" : "-") + "%d", 
-                            (GetRandomNumber() > 50 ? "" : "-") + "x/" + (GetRandomNumber() > 50 ? "" : "-") + 
-                            "%d " + (GetRandomNumber() > 50 ? "+" : "-") + " " + (GetRandomNumber() > 50 ? "" : "-") + 
-                            "%d = " + (GetRandomNumber() > 50 ? "" : "-") + "%d",
-                            (GetRandomNumber() > 50 ? "" : "-") + "(" + (GetRandomNumber() > 50 ? "" : "-") + 
-                            "x " + (GetRandomNumber() > 50 ? "+" : "-") + " "  + (GetRandomNumber() > 50 ? "" : "-") + 
-                            "%d)/" + (GetRandomNumber() > 50 ? "" : "-") + 
-                            "%d = " + (GetRandomNumber() > 50 ? "" : "-") + "%d",
-                            (GetRandomNumber() > 50 ? "" : "-") + "%d(" + (GetRandomNumber() > 50 ? "" : "-") + 
-                            "x " + (GetRandomNumber() > 50 ? "+" : "-") + " " + (GetRandomNumber() > 50 ? "" : "-") + 
-                            "%d) = " + (GetRandomNumber() > 50 ? "" : "-") +"%d",
-                            "%d/x = %d"};
+        (GetRandomNumber() > 50 ? "" : "-") + "%d = " + (GetRandomNumber() > 50 ? "" : "-") + "%d", 
+        (GetRandomNumber() > 50 ? "" : "-") + "x/" + (GetRandomNumber() > 50 ? "" : "-") + 
+        "%d " + (GetRandomNumber() > 50 ? "+" : "-") + " " + (GetRandomNumber() > 50 ? "" : "-") 
+        + "%d = " + (GetRandomNumber() > 50 ? "" : "-") + "%d",
+        (GetRandomNumber() > 50 ? "" : "-") + "(" + (GetRandomNumber() > 50 ? "" : "-") + 
+        "x " + (GetRandomNumber() > 50 ? "+" : "-") + " "  + (GetRandomNumber() > 50 ? "" : "-") + 
+        "%d)/" + (GetRandomNumber() > 50 ? "" : "-") + "%d = " + (GetRandomNumber() > 50 ? "" : "-") + "%d",
+        (GetRandomNumber() > 50 ? "" : "-") + "%d(" + (GetRandomNumber() > 50 ? "" : "-") + 
+        "x " + (GetRandomNumber() > 50 ? "+" : "-") + " " + (GetRandomNumber() > 50 ? "" : "-") + 
+        "%d) = " + (GetRandomNumber() > 50 ? "" : "-") +"%d", "%d/x = %d"};
+
+        // Formats question with chance of operator and digits
         return String.format(twostep[(new Random()).nextInt(twostep.length)], GetRandomInt(), GetRandomInt(), GetRandomInt());
     }
 
+    /**
+     * Array of all multi step types of questions
+     * @return String
+     */
     public String MultiStepQuestions() {
         String[] multistep = {(GetRandomNumber() > 50 ? "" : "-") + "%d/" + (GetRandomNumber() > 50 ? "" : "-") + 
-                            "%dx " + (GetRandomNumber() > 50 ? "+" : "-") + 
-                            " " + (GetRandomNumber() > 50 ? "" : "-") + "%d = " + (GetRandomNumber() > 50 ? "" : "-") + "%d", 
-                            (GetRandomNumber() > 50 ? "" : "-") + "%d/" + (GetRandomNumber() > 50 ? "" : "-") + "%dx " + 
-                            (GetRandomNumber() > 50 ? "+" : "-") + 
-                            " " + (GetRandomNumber() > 50 ? "" : "-") + "%d = " + (GetRandomNumber() > 50 ? "" : "-") + "%d/" + 
-                            (GetRandomNumber() > 50 ? "" : "-") + "%d", (GetRandomNumber() > 50 ? "" : "-") + "%d(" + (GetRandomNumber() > 50 ? "" : "-") + 
-                            "%dx " + (GetRandomNumber() > 50 ? "+" : "-") + 
-                            " " + (GetRandomNumber() > 50 ? "" : "-") + "%d)/" + (GetRandomNumber() > 50 ? "" : "-") + "%d = %d", 
-                            (GetRandomNumber() > 50 ? "" : "-") + "%d/" + (GetRandomNumber() > 50 ? "" : "-") + "%dx " + (GetRandomNumber() > 50 ? "+" : "-") + 
-                            " " + (GetRandomNumber() > 50 ? "" : "-") + "%d/" + (GetRandomNumber() > 50 ? "" : "-") + "%d = " + (GetRandomNumber() > 50 ? "" : "-") + 
-                            "%d", (GetRandomNumber() > 50 ? "" : "-") + "%d/" + (GetRandomNumber() > 50 ? "" : "-") + "%dx " + (GetRandomNumber() > 50 ? "+" : "-") + 
-                            " " + (GetRandomNumber() > 50 ? "" : "-") + "%d/" + (GetRandomNumber() > 50 ? "" : "-") + "%d = " + (GetRandomNumber() > 50 ? "" : "-") + 
-                            "%d/" + (GetRandomNumber() > 50 ? "" : "-") + "%d",  (GetRandomNumber() > 50 ? "" : "-") + "%d/x = " + (GetRandomNumber() > 50 ? "" : "-") + 
-                            "%d/" + (GetRandomNumber() > 50 ? "" : "-") + "%d", "%d/%dx " + (GetRandomNumber() > 50 ? "+" : "-") + " %d/%d = %d/%d", 
-                            (GetRandomNumber() > 50 ? "" : "-") + "%d/x " + (GetRandomNumber() > 50 ? "+" : "-") + " " + (GetRandomNumber() > 50 ? "" : "-") + 
-                            "%d/%d = %d/%d"};
+        "%dx " + (GetRandomNumber() > 50 ? "+" : "-") + 
+        " " + (GetRandomNumber() > 50 ? "" : "-") + "%d = " + (GetRandomNumber() > 50 ? "" : "-") + "%d", 
+        (GetRandomNumber() > 50 ? "" : "-") + "%d/" + (GetRandomNumber() > 50 ? "" : "-") + "%dx " + 
+        (GetRandomNumber() > 50 ? "+" : "-") + 
+        " " + (GetRandomNumber() > 50 ? "" : "-") + "%d = " + (GetRandomNumber() > 50 ? "" : "-") + "%d/" + 
+        (GetRandomNumber() > 50 ? "" : "-") + "%d", (GetRandomNumber() > 50 ? "" : "-") + "%d(" + (GetRandomNumber() > 50 ? "" : "-") + 
+        "%dx " + (GetRandomNumber() > 50 ? "+" : "-") + 
+        " " + (GetRandomNumber() > 50 ? "" : "-") + "%d)/" + (GetRandomNumber() > 50 ? "" : "-") + "%d = %d", 
+        (GetRandomNumber() > 50 ? "" : "-") + "%d/" + (GetRandomNumber() > 50 ? "" : "-") + "%dx " + (GetRandomNumber() > 50 ? "+" : "-") + 
+        " " + (GetRandomNumber() > 50 ? "" : "-") + "%d/" + (GetRandomNumber() > 50 ? "" : "-") + "%d = " + (GetRandomNumber() > 50 ? "" : "-") + 
+        "%d", (GetRandomNumber() > 50 ? "" : "-") + "%d/" + (GetRandomNumber() > 50 ? "" : "-") + "%dx " + (GetRandomNumber() > 50 ? "+" : "-") + 
+        " " + (GetRandomNumber() > 50 ? "" : "-") + "%d/" + (GetRandomNumber() > 50 ? "" : "-") + "%d = " + (GetRandomNumber() > 50 ? "" : "-") + 
+        "%d/" + (GetRandomNumber() > 50 ? "" : "-") + "%d",  (GetRandomNumber() > 50 ? "" : "-") + "%d/x = " + (GetRandomNumber() > 50 ? "" : "-") + 
+        "%d/" + (GetRandomNumber() > 50 ? "" : "-") + "%d", "%d/%dx " + (GetRandomNumber() > 50 ? "+" : "-") + " %d/%d = %d/%d", 
+        (GetRandomNumber() > 50 ? "" : "-") + "%d/x " + (GetRandomNumber() > 50 ? "+" : "-") + " " + (GetRandomNumber() > 50 ? "" : "-") + 
+        "%d/%d = %d/%d"};
 
+        // Formats question with chance of operator and digits
         return String.format(multistep[(new Random()).nextInt(multistep.length)], GetRandomInt(), GetRandomInt(), GetRandomInt(), GetRandomInt(), GetRandomInt(), GetRandomInt(), GetRandomInt());
     }
 
+    /**
+     *  Checks whether user answer is equal to actual answer of question
+     * @param question
+     * @param answer
+     * @return boolean
+     */
     public static boolean CheckAnswer(String question, String answer) {
         LinearEquation ans = new LinearEquation(question);
         boolean flag = false;
-        if(LinearEquation.answer.equals(answer)) {
+
+        // If answer of user is same as actual answer
+        if(LinearEquation.xVal.equals(answer)) {
+            // Answer of user is correct
             flag = true;
         } else {
+            // Otherwise answer of user is wrong
             flag = false;
         }
+        // Return if answer is right or wrong
         return flag;
     }
 }
