a0a3d137c1b6624fa66487ed9818371536af07ed
cylify <mradi.lachman@gmail.com>
2022-11-07 19:09:30 -0500

    Added all possible left sides and right sides

--- InputValidation.java
+++ InputValidation.java
@@ -1,3 +1,60 @@
 class InputValidator {
-    
+
+    /**
+     * Checks if user expression is valid or not
+     * @param leftSide
+     * @param rightSide
+     * @return boolean
+     */
+    public boolean CheckForExpression(String leftSide, String rightSide) {
+        boolean flag = true;
+        String frac = "((-{0,1}[1-9][0-9]*)|-{0,1}0)/-{0,1}[1-9][0-9]*";
+        String wholeFrac = "(-{0,1}[1-9][0-9]*)|-{0,1}0";
+
+        // Checks is rightside is valid or not
+        if(rightSide.isEmpty()) {
+            flag = false;
+        } else if(!(rightSide.matches(wholeFrac) || rightSide.matches(frac))) {
+            flag = false;
+        }
+
+        // Call method to get all possible variants of left side
+        String[] possibleLeftSide = possibleLeftSides();
+
+        if(leftSide.contains("(")) {
+            for(int i = 0; i < possibleLeftSide.length; i++) {
+                if(!(leftSide.matches(possibleLeftSide[i]))) {
+                    flag = false;
+                    if(flag) {
+                        break;
+                    } else {
+                        continue;
+                    }
+                }
+            }
+        } else {
+
+        }
+
+        return flag;
+
+    }
+
+    /**
+     * Array of all possible left sides
+     * @return String[]
+     */
+    String[] possibleLeftSides() {
+        String[] possibleExpr = {"\\(x (\\+|-) -?[1-9][0-9]*\\)/-?[1-9][0-9]*", "-?[1-9][0-9]*/-?x",
+        "-?[1-9][0-9]*", "-?[1-9][0-9]*/-?[1-9][0-9]*","-?[1-9][0-9]*x", "-?x-?[1-9][0-9]",
+        "-?x-?[1-9][0-9]*/-?[1-9][0-9]*","-?[1-9][0-9]*/-?[1-9][0-9]*x", "-?[1-9][0-9]*\\(x (\\+|-) -?[1-9][0-9]*\\)",
+        "-?[1-9][0-9]*\\(x (\\+|-) -?[1-9][0-9]*/-?[1-9][0-9]*\\)", "-?[1-9][0-9]*\\(x (\\+|-) -?[1-9][0-9]*/-?[1-9][0-9]*\\)/-?[1-9][0-9]*",
+        "-?[1-9][0-9]*/-?[1-9][0-9]*\\(x (\\+|-) -?[1-9][0-9]*)", "-?[1-9][0-9]*/-?[1-9][0-9]*\\(x (\\+|-) -?[1-9][0-9]*/-?[1-9][0-9]*\\)",
+        "-?[1-9][0-9]*/-?[1-9][0-9]*\\(x (\\+|-) -?[1-9][0-9]*/-?[1-9][0-9]*\\)/-?[1-9][0-9]*",
+        "\\(x (\\+|-) -?[1-9][0-9]*/-?[1-9][0-9]*\\)/-?[1-9][0-9]*","-?[1-9][0-9]\\(x (\\+|-) -?[1-9][0-9]*/-?[1-9][0-9]*\\)/-?[1-9][0-9]*",
+        "\\(-?[1-9][0-9]*x (\\+|-) -?[1-9][0-9]*/-?[1-9][0-9]*\\)/-?[1-9][0-9]*", 
+        "\\(-?[1-9][0-9]*/-?[1-9][0-9]*x (\\+|-) -?[1-9][0-9]*/-?[1-9][0-9]*\\)/-?[1-9][0-9]*","-?x/-?[1-9][0-9]*"};
+        return possibleExpr;
+
+    }
 }
